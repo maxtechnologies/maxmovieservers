9b99dabde8f4ccdfc31533bc2a27e3fd5ba284c8
maxtechnologies <maxtechnologies124@gmail.com>
2026-05-09 14:41:56 +0300

    Create info.js

---
+++ api/info.js
@@ -0,0 +1,46 @@
+const apiClient = require('../utils/apiClient');
+
+module.exports = async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'GET') {
+    return res.status(405).json({
+      status: 405,
+      success: false,
+      message: 'Method not allowed. Use GET.'
+    });
+  }
+
+  try {
+    const { id } = req.query;
+    
+    if (!id) {
+      return res.status(400).json({
+        status: 400,
+        success: false,
+        message: 'ID parameter is required'
+      });
+    }
+
+    const data = await apiClient.get(`/info/${id}`);
+    
+    return res.status(200).json({
+      status: 200,
+      success: true,
+      creator: "MaxMovies",
+      ...data
+    });
+    
+  } catch (error) {
+    return res.status(error.status || 500).json({
+      status: error.status || 500,
+      success: false,
+      message: error.message || 'Internal server error'
+    });
+  }
+};
